a3c8f345c9c66568acf5345f6d786d68a44003c0
MohabAdnaan <41129300+MohabAdnaan@users.noreply.github.com>
2018-07-12 15:57:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -417,24 +417,16 @@ client.on('message', function(msg) {
       msg.channel.send({embed: embed})
   }
 });
-   client.on("message", message => {
-    const prefix = "$"
-              
-          if(!message.channel.guild) return;
-   if(message.author.bot) return;
-      if(message.content === prefix + "image"){ 
-          const embed = new Discord.RichEmbed()
-  
-      .setTitle(`This is  ** ${message.guild.name} **  Photo !`)
-  .setAuthor(message.author.username, message.guild.iconrURL)
-    .setColor(0x164fe3)
-    .setImage(message.guild.iconURL)
-    .setURL(message.guild.iconrURL)
-                    .setTimestamp()
-
-   message.channel.send({embed});
-      }
-  });
+client.on('message', message => {
+    if (message.content.startsWith("$stats")) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .addField('Uptime', timeCon(process.uptime()), true)
+            .addField('RAM Usage', `${(process.memoryUsage().rss / 1048576).toFixed()}MB`, true)
+            .addField('Guild Count', client.guilds.size, true)
+    })
+}
+});
 
 
 
