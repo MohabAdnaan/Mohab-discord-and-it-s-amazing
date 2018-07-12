c90b0314bc112c85bc91395fb10e4b1455dc8139
MohabAdnaan <41129300+MohabAdnaan@users.noreply.github.com>
2018-07-12 09:58:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -225,7 +225,6 @@ client.on('message', message => {
     }
 });
 //server-avatar
-var prefix = "+";
 client.on("message", message => {    
     if(!message.channel.guild) return;
 if(message.author.bot) return;
@@ -242,7 +241,6 @@ message.channel.send({embed});
 });
 
 //avatar
-var prefix = "+";
 client.on('message', message => {
 if (message.content.startsWith(prefix + "avatar")) {
   var mentionned = message.mentions.users.first();
@@ -259,7 +257,26 @@ if(mentionned){
 message.channel.sendEmbed(embed);
 }
 });
-
+client.on('message',async function (message) =>  {
+const prefix = "+";
+if (message.content.startsWith(prefix+'#'))
+    {
+        var members = []
+        let evidence = message.content.split(" ").slice(1,2).join(" ")
+        let reason = message.content.split(" ").slice(2).join(" ")
+        if (!reason) return message.reply(`**${prefix}apply [تقديمك] [رابط مقطعك]**`)
+        if(!evidence.match(linkreg)) return message.channel.send(`**${prefix}apply [تقديمك] [رابط مقطعك]**`)
+        if(!evidence) return message.reply(`راجاً ضع رابط مقطع لتقديمك`)
+        var embed = new Discord.RichEmbed()
+            .setTitle(`تقديم من ${message.author.username}`)
+            .addField(`التقديم`, "**`\`\`\"+ reason + "`\`\`\**")
+            .addField(`رابط المقطع`, evidence)
+            .setColor(`GREEN`)
+            client.channels.get("حط ايدي الشنل هنا").send(embed)
+            members.push(message.author.id);
+            message.channel.send(`${mentions} تم تقديم طلبك...`)
+        }  
+});
 
 
 
