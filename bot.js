61445f4bb4f383fa488763b15e75248c3e8e7709
MohabAdnaan <41129300+MohabAdnaan@users.noreply.github.com>
2018-07-12 08:19:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,6 +52,21 @@ client.on('message', msg => {
   if(msg.content === 'السلام عليكم')
   msg.reply('عليكم السلام ')
 });
+client.on('message', message => {
+if (message.content.startsWith("ban")) {
+    var mention = message.mentions.members.first();
+    if(!mention) return message.channel.send("يجب منشن العضو");
+
+    mention.ban("By: " + message.author.tag);
+    
+    message.channel.send("تم أعطاء باند الى : " + mention.tag);
+};
+});
+
+
+
+
+
 
 
 
